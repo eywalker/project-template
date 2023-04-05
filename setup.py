4430814c33cc0e4c7e444a5131eb6808aff874eb
Edgar Y. Walker <eywalker@users.noreply.github.com>
2023-04-05 22:01:34 +0000

    Update setup to use requirements files

--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
+# get regular requirements from requirements.txt
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+# get dev requirements from dev-requirements.txt
+with open('dev-requirements.txt') as f:
+    dev_requirements = f.read().splitlines()
+
 setup(
     name="my_project",
     version="0.0.0",
     description='My project description',
     packages=find_packages(),
+    install_requires=requirements,
+    extras_require={
+        'dev': dev_requirements,
+    },
 )
